31e11104f37d1feffaeead64edfe8a40c2242406
Viviana Garcia <vivianandrea4@gmail.com>
2020-11-02 16:44:16 -0500

    vivi

--- src/movies.js
+++ src/movies.js
@@ -1,7 +1,11 @@
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors. How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors(movies) {
-    return movies.map(x => x.director)
+function getAllDirectors(boppityBloopBlahBloop) {
+    return boppityBloopBlahBloop.map(jellyBean => {
+        return jellyBean.director
+    })
+
+    console.log(jellyBean)
     // let directors =[]
     // for(let movie of movies){
     //     directors.push(movie.director)
@@ -10,6 +14,15 @@ function getAllDirectors(movies) {
     // return directors
 }
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
+
+let howManyMovies = (movies) => {
+    let dramaSpielberg = movies.filter((eachMovie) => {
+        return eachMovie.director === "Steven Spielberg" && eachMovie.genre.includes("Drama")
+
+    })
+    return dramaSpielberg.length
+
+}
 
 
 // Iteration 3: All rates average - Get the average of all rates with 2 decimals
